59fe898124bbc1ccbdb61371641b8eba0ba34202
shrawankansi <shrawankansi@gmail.com>
2023-01-21 00:18:29 +0530

    update

--- src/context/contextApi.js
+++ src/context/contextApi.js
@@ -4,7 +4,7 @@ import {fetchDataFromApi} from "../utils/api";
 
 export const Context = createContext();
 
-export const AppConstext = (props)  => {
+export const AppContext = (props)  => {
     const [loading, setLoading]= useState(false);
     const [searchResults, setSearchResults] = useState(false);
     const [selectCategories, setSelectCategories] = useState("New");
